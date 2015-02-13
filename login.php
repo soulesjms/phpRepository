be6a0ade805a9ca420f0dabf1aec4bfb3f456fce
soulesjms <soulesjms@gmail.com>
2015-02-12 22:35:41 -0700

    changed dbName and the test statement

--- login.php
+++ login.php
@@ -13,10 +13,11 @@ $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbName = "directory";
 
 echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 
-$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 //form data
 $gender = $_GET['gender'];
