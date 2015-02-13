6e0c2708655f40524eb531876fa1fc9a4de04187
soulesjms <soulesjms@gmail.com>
2015-02-12 22:38:32 -0700

    changed password manually

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ else {
 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbPassword = "php-pass";//getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $dbName = "directory";
 
 echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
