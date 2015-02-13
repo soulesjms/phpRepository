a54ebb0da160c384498c7659a8f3c7d2564e12f0
soulesjms <soulesjms@gmail.com>
2015-02-12 22:24:54 -0700

    adding test statements and removed port

--- login.php
+++ login.php
@@ -14,8 +14,9 @@ $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 
-$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 
+$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
 
 //form data
 $gender = $_GET['gender'];
