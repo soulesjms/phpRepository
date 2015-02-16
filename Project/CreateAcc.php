c5009ccd2575aa7e9f4da22918372d086ccf68dc
soulesjms <soulesjms@gmail.com>
2015-02-15 18:33:54 -0700

    adding test statements for openshift database

--- Project/CreateAcc.php
+++ Project/CreateAcc.php
@@ -1,4 +1,20 @@
 <?php
+$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+if ($openShiftVar === null || $openShiftVar == "") {
+	$dbHost = "localhost";
+}
+else {
+     $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+}
+
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbName = "directory";
+
+echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
+
 if(isset($_POST['username'])){
 	$username = $_POST['username'];
     $password = $_POST['password'];
@@ -11,13 +27,13 @@ if(isset($_POST['username'])){
     {
        $hostuser = "php";
        $hostpassword = "php-pass";
-       $db = new PDO("mysql:host=localhost;dbname=scriptures", $hostuser, $hostpassword);
+       $db = new PDO("mysql:host=localhost;dbname=directory", $hostuser, $hostpassword);
     }
     catch (PDOException $ex) {
         echo "Error!: " . $ex->getMessage();
         die();	
 	}
-    $sql = "INSERT INTO users ('username', 'password', 'firstName', 'lastName', 'major', 'apartment') VALUES ('$username', '$password', '$firstName', '$lastName', '$major', '$apartment');";
+    $sql = "INSERT INTO users (username, password, firstName, lastName, major, apartment) VALUES ('$username', '$password', '$firstName', '$lastName', '$major', '$apartment');";
 	$temp = $db->query($sql);
 	$sql = "";
 }
